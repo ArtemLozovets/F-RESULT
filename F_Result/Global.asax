d2a344933c64e86c866697f0ea2f7fc6121d9f0b
ArtemLozovets <lozovec80@gmail.com>
2017-03-18 02:26:10 +0200

    first commit

---
+++ F_Result/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="F_Result.MvcApplication" Language="C#" %>
---
+++ F_Result/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(F_Result.Startup))]
+namespace F_Result
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
