72762397f7af5531c2febec3162fa67b824ad3a6
ArtemLozovets <lozovec80@gmail.com>
2017-07-12 12:02:21 +0300

    AnalysisOfPlanPayments Add

--- F_Result/Controllers/ReportsController.cs
+++ F_Result/Controllers/ReportsController.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
 
 namespace F_Result.Controllers
 {
@@ -14,11 +12,13 @@ namespace F_Result.Controllers
         private FRModel db = new FRModel();
 
         // GET: Reports
+        [Authorize(Roles = "Administrator, Chief, Accountant")]
         public ActionResult AnalysisOfPayments()
         {
             return View();
         }
 
+        [Authorize(Roles = "Administrator, Chief, Accountant")]
         public JsonResult GetAOP(int? Year)
         {
             db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
@@ -36,7 +36,7 @@ namespace F_Result.Controllers
                             {
                                 Month = g.Key.Month,
                                 Total = g.Sum(t => t.Payment)
-                            });
+                            }).ToList();
 
             List<decimal?> _insum = new List<decimal?>();
             for (int i = 1; i < 13; i++)
@@ -56,7 +56,7 @@ namespace F_Result.Controllers
                               {
                                   Month = g.Key.Month,
                                   Total = g.Sum(t => t.Sum)
-                              });
+                              }).ToList();
 
             List<decimal?> _outsum = new List<decimal?>();
             for (int i = 1; i < 13; i++)
@@ -69,9 +69,72 @@ namespace F_Result.Controllers
             }
 
 
-            return Json(new { Result = true, Message = String.Format("График платежей за {0} год", Year), ChartData = _insum, ChartDataA = _outsum }, JsonRequestBehavior.AllowGet);
+            return Json(new { Result = true, ChartData = _insum, ChartDataA = _outsum }, JsonRequestBehavior.AllowGet);
 
         }
+
+        [Authorize(Roles = "Administrator, Chief, Accountant")]
+        public ActionResult AnalysisOfPlanPayments()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator, Chief, Accountant")]
+        public JsonResult GetPlanData(int? Year)
+        {
+            db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+            if (Year == null)
+            {
+                Year = DateTime.Today.Year;
+            }
+
+            // График плановых доходов -------------------------------------------
+            var _pclist = (from t in db.PlanCredits
+                              group t by new { t.Date.Year, t.Date.Month } into g
+                              where g.Key.Year == Year
+                              select new
+                              {
+                                  Month = g.Key.Month,
+                                  Total = g.Sum(t => t.Sum)
+                              }).ToList();
+
+            List<decimal?> _pcsum = new List<decimal?>();
+            for (int i = 1; i < 13; i++)
+            {
+                if (_pclist.FirstOrDefault(x => x.Month == i) != null)
+                {
+                    _pcsum.Add(_pclist.FirstOrDefault(x => x.Month == i).Total);
+                }
+                else _pcsum.Add(0);
+            }
+
+            // График плановых расходов -------------------------------------------
+            var _pdlist = (from t in db.PlanDebits
+                               group t by new { t.Date.Year, t.Date.Month } into g
+                               where g.Key.Year == Year
+                               select new
+                               {
+                                   Month = g.Key.Month,
+                                   Total = g.Sum(t => t.Sum)
+                               }).ToList();
+
+            List<decimal?> _pdsum = new List<decimal?>();
+            for (int i = 1; i < 13; i++)
+            {
+                if (_pdlist.FirstOrDefault(x => x.Month == i) != null)
+                {
+                    _pdsum.Add(_pdlist.FirstOrDefault(x => x.Month == i).Total);
+                }
+                else _pdsum.Add(0);
+            }
+
+
+            return Json(new { Result = true, ChartData = _pcsum, ChartDataA = _pdsum }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
