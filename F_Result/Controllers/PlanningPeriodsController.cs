f4c44da7428873701c5de696a420b1b23094ea32
ArtemLozovets <lozovec80@gmail.com>
2017-09-06 23:14:35 +0300

    PlanningPeriod work

--- F_Result/Controllers/PlanningPeriodsController.cs
+++ F_Result/Controllers/PlanningPeriodsController.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Linq.Dynamic; //!=====!
 using System.Web.Mvc;
 using F_Result.Models;
+using System;
 
 namespace F_Result.Controllers
 {
@@ -13,21 +14,73 @@ namespace F_Result.Controllers
 
         [Authorize(Roles = "Administrator, Chief, Accountant")]
         // GET: PlanningPeriods
-        public ActionResult Index()
-        {
-            return View(db.PlanningPeriods.ToList());
-        }
-
-        [Authorize(Roles = "Administrator, Chief, Accountant")]
-        // GET: PlanningPeriods
         public ActionResult PPShow()
         {
             return View();
         }
 
+
+        [Authorize(Roles = "Administrator, Chief, ProjectManager, Accountant")]
+        [HttpPost]
+        public ActionResult LoadPP()
+        {
+            try
+            {
+                db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s)); //Debug Information====================
+
+                var draw = Request.Form.GetValues("draw").FirstOrDefault();
+                var start = Request.Form.GetValues("start").FirstOrDefault();
+                var length = Request.Form.GetValues("length").FirstOrDefault();
+                var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+                var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int totalRecords = 0;
+
+                string _ppid = Request.Form.GetValues("columns[0][search][value]").FirstOrDefault().ToString();
+                string _ppname = Request.Form.GetValues("columns[1][search][value]").FirstOrDefault().ToString();
+
+                var _pperiod = (from period in db.PlanningPeriods
+                            where (period.PeriodName.Contains(_ppname) || String.IsNullOrEmpty(_ppname))
+                            select new
+                            {
+                                PlanningPeriodId = period.PlanningPeriodId,
+                                PeriodName = period.PeriodName
+                            }).AsEnumerable().Select(x => new PlanningPeriod
+                            {
+                                PlanningPeriodId = x.PlanningPeriodId,
+                                PeriodName = x.PeriodName
+                            }).ToList();
+
+                _pperiod = _pperiod.Where(x => (x.PlanningPeriodId.ToString().Contains(_ppid)) || string.IsNullOrEmpty(_ppid)).ToList();
+
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+                {
+                    _pperiod = _pperiod.OrderBy(sortColumn + " " + sortColumnDir).ToList();
+                }
+                else
+                {
+                    _pperiod = _pperiod.OrderBy(x => x.PlanningPeriodId).ToList();
+                }
+
+                totalRecords = _pperiod.Count();
+
+                var data = _pperiod.Skip(skip).Take(pageSize);
+                return Json(new { draw = draw, recordsFiltered = totalRecords, recordsTotal = totalRecords, data = data, errormessage = "" }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                var errormessage = "Ошибка выполнения запроса!\n\r" + ex.Message + "\n\r" + ex.StackTrace;
+                var data = "";
+                return Json(new { data = data, errormessage = errormessage }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
         // GET: PlanningPeriods/Create
         [Authorize(Roles = "Administrator, Chief, Accountant")]
-        public ActionResult Create()
+        public ActionResult PPCreate()
         {
             return View();
         }
@@ -36,13 +89,13 @@ namespace F_Result.Controllers
         [Authorize(Roles = "Administrator, Chief, Accountant")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PlanningPeriodId,PeriodName")] PlanningPeriod planningPeriod)
+        public ActionResult PPCreate([Bind(Include = "PlanningPeriodId,PeriodName")] PlanningPeriod planningPeriod)
         {
             if (ModelState.IsValid)
             {
                 db.PlanningPeriods.Add(planningPeriod);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("PPShow");
             }
 
             return View(planningPeriod);
@@ -50,7 +103,7 @@ namespace F_Result.Controllers
 
         // GET: PlanningPeriods/Edit/5
         [Authorize(Roles = "Administrator, Chief, Accountant")]
-        public ActionResult Edit(int? id)
+        public ActionResult PPEdit(int? id)
         {
             if (id == null)
             {
@@ -68,20 +121,20 @@ namespace F_Result.Controllers
         [Authorize(Roles = "Administrator, Chief, Accountant")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PlanningPeriodId,PeriodName")] PlanningPeriod planningPeriod)
+        public ActionResult PPEdit([Bind(Include = "PlanningPeriodId,PeriodName")] PlanningPeriod planningPeriod)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(planningPeriod).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("PPShow");
             }
             return View(planningPeriod);
         }
 
         // GET: PlanningPeriods/Delete/5
         [Authorize(Roles = "Administrator, Chief, Accountant")]
-        public ActionResult Delete(int? id)
+        public ActionResult PPDelete(int? id)
         {
             if (id == null)
             {
@@ -97,14 +150,14 @@ namespace F_Result.Controllers
 
         // POST: PlanningPeriods/Delete/5
         [Authorize(Roles = "Administrator, Chief, Accountant")]
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName("PPDelete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
             PlanningPeriod planningPeriod = db.PlanningPeriods.Find(id);
             db.PlanningPeriods.Remove(planningPeriod);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("PPShow");
         }
 
         protected override void Dispose(bool disposing)
