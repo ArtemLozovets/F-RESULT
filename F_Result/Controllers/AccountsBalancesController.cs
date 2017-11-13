cfab5cb3e2a66bfe61c72d021a1aa3bfe9933742
ArtemLozovets <lozovec80@gmail.com>
2017-11-13 16:59:23 +0200

    FlowPanel modify

--- F_Result/Controllers/AccountsBalancesController.cs
+++ F_Result/Controllers/AccountsBalancesController.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using F_Result.Models;
 using System.Linq.Dynamic; //!=====!
