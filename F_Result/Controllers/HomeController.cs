5c8e7d8d007fdf32263c8b57362b0d9f56ee9b1d
ArtemLozovets <lozovec80@gmail.com>
2018-02-04 15:01:39 +0200

    Update 040218 15:01

--- F_Result/Controllers/HomeController.cs
+++ F_Result/Controllers/HomeController.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using F_Result.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace F_Result.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            if (Request.IsAuthenticated)
+            {
+                //Получаем идентификатор текущего пользователя
+                using (ApplicationDbContext aspdb = new ApplicationDbContext())
+                {
+                    using (RoleManager<ApplicationRole> roleManager = new RoleManager<ApplicationRole>(new RoleStore<ApplicationRole>(aspdb)))
+                    {
+                        var _UserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+                        ApplicationRole currentRole = roleManager.FindById(aspdb.Users.FirstOrDefault(x => x.Id == _UserId).Roles.FirstOrDefault().RoleId);
+
+                        @ViewData["FirstName"] = aspdb.Users.FirstOrDefault(x => x.Id == _UserId).FirstName.ToString();
+                        @ViewData["UserName"] = aspdb.Users.FirstOrDefault(x => x.Id == _UserId).UserName.ToString();
+                        @ViewData["UserRole"] = currentRole.Description.ToString();
+                    }
+                }
+            }
             return View();
         }
 
