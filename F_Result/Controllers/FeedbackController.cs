68d3c7adab447707201d5962faa1644476362b30
ArtemLozovets <lozovec80@gmail.com>
2018-04-04 16:56:53 +0300

    Feedback fn start

---
+++ F_Result/Controllers/FeedbackController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F_Result.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult CommentsShow(string cUrl)
+        {
+            if (!string.IsNullOrEmpty(cUrl))
+            {
+                ViewData["cUrl"] = cUrl;
+            }
+            return View();
+        }
+    }
+}
---
+++ F_Result/Models/Feedback.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace F_Result.Models
+{
+    [Table("Feedback")]
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }
+
+        [StringLength(500, ErrorMessage = "Отзыв должен содержать не более 500 символов")]
+        [Display(Name = "Отзыв")]
+        public string Comment { get; set; }
+
+        [StringLength(128, ErrorMessage = "Ссылка должна содержать не более 128 символов")]
+        [Display(Name = "Ссылка")]
+        public string SbUrl { get; set; }
+
+        [Display(Name = "Дата добавления")]
+        public DateTime DateOfCreation { get; set; }
+
+        public string UserId { get; set; }
+
+        public string ApprovedUserId { get; set; }
+
+        [Display(Name = "Статус")]
+        public short Status { get; set; }
+
+        [Display(Name = "Дата рассмотрения")]
+        public DateTime DateOfApproved { get; set; }
+
+        //------------------------------------------------------
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual ApplicationUser ApplicationUser1 { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Пользователь")]
+        public string UserFN { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Рассмотрено")]
+        public string ApprovedUserFN { get; set; }
+
+    }
+}
